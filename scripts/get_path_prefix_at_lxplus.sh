4bad06b23e0b8f7f82d794cacb215e4c47e8ba40
vkhristenko <vdkhristenko1991@gmail.com>
2024-10-11 00:35:42 +0200

    committing

--- scripts/get_path_prefix_at_lxplus.sh
+++ scripts/get_path_prefix_at_lxplus.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "/cvmfs/sft.cern.ch/lcg/releases/LCG_103/ROOT/6.28.00/x86_64-centos9-gcc11-opt/;/cvmfs/sft.cern.ch/lcg/releases/LCG_103/Boost/1.81.0/x86_64-centos9-gcc11-opt/"
+echo "/cvmfs/sft.cern.ch/lcg/releases/LCG_103/ROOT/6.28.00/x86_64-centos9-gcc11-opt;/cvmfs/sft.cern.ch/lcg/releases/LCG_103/Boost/1.81.0/x86_64-centos9-gcc11-opt"
